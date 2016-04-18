8e99715e16fe4e2290a9ee2d2368f38116d5029e
Hernan Zbucki <Hernan Zbucki>
2016-04-17 23:16:47 -0300

    Agrego pruebas unitarias de Facade

---
+++ TransactionKernelDSL.Framework.UnitTests/Facade/FacadeUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TransactionKernelDSL.Framework.UnitTests.Facade.Mocks;
+using TransactionKernelDSL.Framework.V1;
+
+namespace TransactionKernelDSL.Framework.UnitTests.Facade
+{
+    [TestClass]
+    public class FacadeUnitTest
+    {
+        [TestMethod]
+        public void CanInstantiateMockFacade()
+        {
+            ITransactionFacade facade = new MockFacade();
+            
+            Assert.AreEqual(facade.InstanceId, 99);
+            Assert.AreEqual(facade.StartEngines(), true);
+            Assert.AreEqual(facade.StopEngines(), true);
+
+        }
+    }
+}
---
+++ TransactionKernelDSL.Framework.UnitTests/Facade/Mocks/MockFacade.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionKernelDSL.Framework.V1;
+
+namespace TransactionKernelDSL.Framework.UnitTests.Facade.Mocks
+{
+    public class MockFacade : ITransactionFacade
+    {
+        public int InstanceId
+        {
+            get { return 99; }
+        }
+
+        public bool StartEngines()
+        {
+            return true;
+        }
+
+        public bool StopEngines()
+        {
+            return true;
+        }
+    }
+}
