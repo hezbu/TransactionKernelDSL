5549c27b51fa464fb69541384e58dc242dde0639
Hernan Zbucki <Hernan Zbucki>
2016-04-17 23:04:02 -0300

    Armando carpeta V2 y agregando primer archivo de interfaces

---
+++ TransactionKernelDSL.Framework/V2/Interfaces/ITransactionFacade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace TransactionKernelDSL.Framework.V1
+{
+    public interface ITransactionFacade
+    {
+        /// <summary>
+        /// Gets the Instance Id assigned to the Transactional Facade
+        /// </summary>
+        int InstanceId { get; }              
+        /// <summary>
+        /// Starts every engine bound to the Transactional Facade
+        /// </summary>
+        /// <returns>True if operation was succesful, otherwise false</returns>
+        bool StartEngines();
+        /// <summary>
+        /// Stops every engine bound to the Transactional Facade
+        /// </summary>
+        /// <returns>True if operation was succesful, otherwise false</returns>
+        bool StopEngines();
+    }
+}
