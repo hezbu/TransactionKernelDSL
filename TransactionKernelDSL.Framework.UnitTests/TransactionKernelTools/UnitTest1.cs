a9512ea48800f1b7053c032ed6ab9b0d026448be
Hernan Zbucki <Hernan Zbucki>
2016-04-17 23:36:51 -0300

    Agregado de la clase static para los tools

---
+++ TransactionKernelDSL.Framework.UnitTests/TransactionKernelTools/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TransactionKernelDSL.Framework.V2;
+
+namespace TransactionKernelDSL.Framework.UnitTests.TransactionKernelTools
+{
+    [TestClass]
+    public class TransactionKernelToolsUnitTest
+    {
+        
+    }
+}
